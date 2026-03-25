0bd1a675553b4609d15251524297e82ecf05f78f
Sumit Gupta <cricsumit24@gmail.com>
2026-03-25 11:30:18 +0530

    cors policy changed

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cors({
   origin: function (origin, callback) {
     const allowed = [
       'https://task-manager-mu-rose.vercel.app',
-      'http://localhost:5173',
+      'http://localhost:3000',
     ];
     if (!origin || allowed.includes(origin)) {
       callback(null, true);
